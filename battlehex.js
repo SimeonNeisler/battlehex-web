24bcc9701d17c1c95ebbf2597370742436d24844
Simeon Neisler <simeon.neisler@gmail.com>
2017-08-10 01:30:59 -0400

    updated to use Express

--- battlehex.js
+++ battlehex.js
@@ -1,22 +1,36 @@
-var tiles = document.querySelectorAll(".square");
-var numPlayers = 4;
-var players = [];
-var playerBoxes = document.querySelectorAll(".player");
-const player = require('./src/player');
-for (i = 0; i < tiles.length; i++) {
-  tiles[i].textContent = ("Tile " + (i+1) + "\n");
-  tiles[i].textContent += ("Resources: " + (Math.floor(Math.random() * 3) + 1));
-}
-for (i = 0; i < numPlayers; i++) {
-  name = prompt("Insert Player Name: ");
-  color = prompt("Choose Color: ");
-  newPlayer = player(name, color, 0, 0);
-  player[i] = newPlayer;
-  playerBoxes[i].style.textContent = ("Player: " + name + "\n" + "Resources: " + 0 + "\n" + "Income: " + 0);
-  playerBoxes[i].style.background = (color);
-}
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
-function adjustPlayerResources(playerNum, amount) {
-  players[playerNum-1].adjustResources(amount);
+app.use(bodyParser.urlencoded({extended: true}));
 
-}
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("menu");
+});
+
+app.post("/shop", (req, res) => {
+  res.redirect("/store");
+});
+
+app.get("/store", (req, res) => {
+  res.render("store");
+});
+
+app.get("/store/:product", (req, res) => {
+  res.render("product");
+});
+
+app.post("/newGame", (req, res) => {
+  res.redirect("/createGame");
+});
+
+app.get("/createGame", (req, res) => {
+  res.render("newGame");
+});
+
+
+app.listen(3000, process.env.IP, () => {
+  console.log("Server uplink established");
+});
