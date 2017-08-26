3ac8f789cf2bc384b79f50f36605e2b986dfdeab
Simeon Neisler <simeon.neisler@gmail.com>
2017-08-26 13:21:50 -0400

    Worked on post request to add new cards to the store

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var bodyParser = require("body-parser");
-var express = require("express");
-var firebase = require("firebase");
-var path = require("path");
+var bodyParser = require("body-parser"),
+    express    = require("express"),
+    firebase   = require("firebase"),
+    flash      = require("connect-flash"),
+    path       = require("path");
+
 var app = express();
 
+//Config settings for database
 var config = {
   apiKey : "AIzaSyBzxM1cmlN18AvVIE9SrBVIihn4O5vM8Zg",
   authDomain: "battlehex-web.firebaseapp.com",
@@ -11,11 +14,27 @@ var config = {
   storageBucket: "gs://battlehex-web.appspot.com/"
 }
 
+//Declare + initialize Database
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//app.use(flash());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "/public")));
 
+/*app.use((req, res, next) => {
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});*/
+
+
 app.set("view engine", "ejs");
 
+
+//=======================
+//ROUTES
+//=======================
 
 //Login Page
 app.get("/", (req, res) => {
@@ -29,11 +48,39 @@ app.get("/landing", (req, res) => {
 
 //Store Page
 app.get("/store", (req, res) => {
-  res.render("store");
+  return database.ref('/cards/')
+  res.render("store", {cards: cards});
 });
 
+//Dev screen to create new cards
+app.get("/store/new", (req, res) => {
+  res.render("newCard");
+})
+
+//Adds new card to the store
 app.post("/store", (req, res) => {
-  res.redirect("/store");
+  database.ref('cards/' + req.body.name).set({
+    type: req.body.type,
+    name: req.body.name,
+    class: req.body.class,
+    strength: req.body.strength,
+    hitpoints: req.body.hitpoints,
+    range: req.body.range,
+    moves: req.body.moves,
+    abilities: req.body.abilities,
+    cost: req.body.cost,
+    price: req.body.price,
+    image: req.body.image
+  }, (err) => {
+    if(err) {
+      console.log(err);
+      //req.flash("error", "Something went wrong");
+    } else {
+      console.log("success");
+      //req.flash("Success", "New Card Added");
+      res.redirect("/store");
+    }
+  });
 });
 
 
