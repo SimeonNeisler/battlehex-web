e5e21abe4b1993682a7704880533df444fa2da9e
Simeon Neisler <simeon.neisler@gmail.com>
2017-09-21 15:52:48 -0400

    updated config

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ var app = express();
 
 
 //Config settings for database
-
-
+var config = {
+  apiKey : process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_ID
+}
 //Declare + initialize Database
 var firebaseApp = firebase.initializeApp(config);
 var database = firebase.database();
